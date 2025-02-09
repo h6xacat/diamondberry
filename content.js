fa1e835c2fe57e6974be615b2b1fdf4d793d2e96
hexa.cat <music.hexa.cat@gmail.com>
2025-02-09 02:17:00 -0500

    Update version format in content.js and manifest.json to 1.6.0.A---1.5.1

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Diamondberry
 // @namespace    https://greasyfork.org/hexa.cat
-// @version      1.6---1.5A.1
+// @version      1.6.0.A---1.5.1
 // @description  Utility for GdC.
 // @author       hexa.cat
 // @match        https://chatroom.talkwithstranger.com/*
